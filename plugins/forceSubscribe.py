6f8b4e4cbb99bf4003d7404b4c0f3c5e115bd66d
viperadnan-git <viperadnan@gmail.com>
2020-08-16 13:14:27 +0530

    Fixed Pyrogram v18 changes.

--- plugins/forceSubscribe.py
+++ plugins/forceSubscribe.py
@@ -6,7 +6,7 @@ import sql_helpers.forceSubscribe_sql as sql
 
 
 
-@app.on_callback_query(Filters.callback_data("onButtonPress"))
+@app.on_callback_query(Filters.regex("^onButtonPress$"))
 def onButtonPress(client, cb):
    user_id = cb.from_user.id
    chat_id = cb.message.chat.id
@@ -43,9 +43,9 @@ def SendMsg(client, message):
     )
     client.restrict_chat_member(message.chat.id, user_id, ChatPermissions(can_send_messages=False))
   except pyrogram.errors.exceptions.bad_request_400.ChatAdminRequired:
-    client.send_message(message.chat.id, text=f"I am not an admin in @{CHANNEL_USERNAME}")
+    client.send_message(message.chat.id, text=f"❗ **I am not an admin in @{CHANNEL_USERNAME}**")
   except ValueError:
-    client.send_message(message.chat.id, text=f"I am not an admin in @{CHANNEL_USERNAME}")
+    client.send_message(message.chat.id, text=f"❗ **I am not an admin in @{CHANNEL_USERNAME}**")
 
 
 @app.on_message(Filters.command(["forcesubscribe"]) & ~Filters.private)
@@ -58,26 +58,26 @@ def config(client, message):
       input_str = input_str.replace("@", "")
     except IndexError:
       if sql.fs_settings(chat_id):
-        message.reply_text(f"Force Subscribe is **enabled** in this chat.\nChannel - @{sql.fs_settings(chat_id).channel}")
+        message.reply_text(f"✅ **Force Subscribe is enabled in this chat.**\n__For this [Channel](https://t.me/{sql.fs_settings(chat_id).channel})__", disable_web_page_preview=True)
         return
       else:
-        message.reply_text("Force Subscribe is **disabled** in this chat.")
+        message.reply_text("❌ **Force Subscribe is disabled in this chat.**")
         return
     if input_str.lower() in ("off", "no", "disable"):
       sql.disapprove(chat_id)
-      message.reply_text("Force Subscribe is **Disabled**")
+      message.reply_text("❌ **Force Subscribe is Disabled Successfully.**")
     elif input_str:
       try:
         client.get_chat_member(input_str, "me")
         sql.add_channel(chat_id, input_str)
-        message.reply_text(f"Force Subscribe is **enabled** for this chat\nChannel - @{input_str}")
+        message.reply_text(f"✅ **Force Subscribe is Enabled**\n__Force Subscribe is enabled, all the group members have to subscribe this [channel](https://t.me/{input_str}) in order to send messages in this group.", disable_web_page_preview=True)
       except pyrogram.errors.exceptions.bad_request_400.UserNotParticipant:
-        message.reply_text(f"I am not an admin in @{input_str}\nAdd me on your channel as admin and send the command again.")
+        message.reply_text(f"❗ **Not an Admin in the Channel**\n__I am not an admin in the [channel](https://t.me/{input_str}). Add me as a admin in order to enable ForceSubscribe.__", disable_web_page_preview=True)
       except pyrogram.errors.exceptions.bad_request_400.UsernameNotOccupied:
-        message.reply_text(f"Invalid Channel Username")
+        message.reply_text(f"❗ **Invalid Channel Username.**")
       except ValueError:
-        message.reply_text(f"@{input_str} didn't belongs to a channel.")
+        message.reply_text(f"❗ **Invalid Channel Username.**")
     else:
-      message.reply_text("Something went wrong")
+      message.reply_text("⚠️ **Something went wrong.**")
   else:
-    message.reply_text("You have to be the **Group Creator** to do that.")
+    message.reply_text("❗ **Group Creator Required**\n__You have to be the group creator to do that.__")
